c14beb1438399a17aafcd4ab2252d1ca0493cbbc
Havish <Havish747@gmail.com>
2023-10-30 16:25:51 -0400

    better webserver is complete and now serves html and txt files

--- better_webserver.py
+++ better_webserver.py
@@ -1,6 +1,60 @@
-import socket, sys
+import socket, sys, os
+from typing import Optional
 
-server_port = int(sys.argv[1]) if len(sys.argv) > 1 else 28333
+def get_file_data(file_name:str):
+    try:
+        with open(file_name, "rb") as fp:
+            data = fp.read() # Read entire file
+            file_extension = os.path.splitext(file_name)[-1]
+            file_type = None
+            match file_extension:
+
+                case ".txt": 
+                    file_type = "text/plain"
+
+                case ".html": 
+                    file_type = "text/html"
+            
+        return (data, file_type, len(data))
+    except:
+        return None, None, 0
+
+def build_response(data:Optional[bytes], file_type:Optional[str], file_len:int):
+    encoded_response = None
+
+    match data:
+
+        case None:
+            response_status = "HTTP/1.1 404 Not Found"
+            content_type = "Content-Type: text/plain"
+            content_len = "Content-Length: 13"
+            connection = "Connection: close"
+            content = "404 not found"
+            
+            header = "\r\n".join((response_status,content_type,content_len,connection))
+
+            response = header + "\r\n\r\n" + content + "\r\n\r\n"
+            
+            encoded_response = response.encode("ISO-8859-1")
+
+        case _:
+            response_status = "HTTP/1.1 200 OK"
+            content_type = "Content-Type: " + file_type if file_type else ""
+            content_len = "Content-Length: " + str(file_len)
+            connection = "Connection: close"
+            
+            header = "\r\n".join((response_status,content_type,content_len,connection))
+            response = header + "\r\n\r\n"
+            encoded_response = response.encode("ISO-8859-1")
+
+            encoded_response = encoded_response + data + "\r\n\r\n".encode("ISO-8859-1")
+    
+    
+    return encoded_response
+
+        
+
+server_port = int(sys.argv[1]) if len(sys.argv) > 1 else 33490
 
 server_socket = socket.socket()
 server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -19,18 +73,11 @@ while True:
         if "\r\n\r\n" in decoded: break
         response = new_socket.recv(4096)
     
-    decoded_response = "".join(response_list)
-    header = decoded_response.split("\r\n")[0].split()
+    decoded_request = "".join(response_list)
+    print(decoded_request)
+    header = decoded_request.split("\r\n")[0].split()
     file_name = header[1].split("/")[-1]
-    print(file_name)
-
-    response_status = "HTTP/1.1 200 OK"
-    content_type = "Content-Type: text/plain"
-    content_len = "Content-Length: 6"
-    connection = "Connection: close"
-    content = "Hello!"
-
-    header = "\r\n".join((response_status,content_type,content_len,connection,content)) + "\r\n" + "\r\n" 
-    encoded_header = header.encode("ISO-8859-1") 
-    new_socket.sendall(encoded_header)
+    file_data, file_type, file_len = get_file_data(file_name)
+    encoded_response = build_response(file_data,file_type,file_len)
+    new_socket.sendall(encoded_response)
     new_socket.close()
