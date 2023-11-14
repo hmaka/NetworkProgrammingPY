e900fae724b87ff7023d4e573f486c51f544fdc6
Havish <Havish747@gmail.com>
2023-11-13 23:57:20 -0500

    more progress, constructing the main loop

--- TCP_packet_verifier/tcp_packet_verifier.py
+++ TCP_packet_verifier/tcp_packet_verifier.py
@@ -18,15 +18,17 @@ def get_source_and_destination_ip(file_name:str) -> list[str]:
 def construct_pseudo_header(source_ip:str, destination_ip:str, tcp_length:int) -> bytes:
     source_ip_bytes = ip_to_bytes(source_ip)
     destination_ip_bytes = ip_to_bytes(destination_ip)
-    zero = b'00'
-    protocol = b'06'
+    zero = b'\x00'
+    protocol = b'\x06'
     bytes_needed = (tcp_length.bit_length() + 7) // 8
     return source_ip_bytes + destination_ip_bytes + zero + protocol + tcp_length.to_bytes(bytes_needed)
 
 
-def main():
-    print("running main code!")
-    
+def main(ip_file_name:str, tcp_data_file_name:str):
+    source_ip_address, destination_ip_address = get_source_and_destination_ip(ip_file_name)
+    tcp_data = get_tcp_data(tcp_data_file_name)
+    pseudo_header = construct_pseudo_header(source_ip_address,destination_ip_address, len(tcp_data)) 
+     
 
 
 
@@ -46,7 +48,7 @@ def test():
 
     print("test 2:")
     inputs = ("255.0.255.1", "127.255.0.1", 3490)
-    expected_output = b'ff 00 ff 01 7f ff 00 01 00 06 0d a2'
+    expected_output = b'\xff\x00\xff\x01\x7f\xff\x00\x01\x00\x06\x0d\xa2'
     result = construct_pseudo_header(*inputs)
 
     if expected_output == result:
