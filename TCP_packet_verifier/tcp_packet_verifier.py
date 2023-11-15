722d9e34415e571018cd7a1c5a06423931ecd9ad
Havish <Havish747@gmail.com>
2023-11-14 19:41:28 -0500

    "done" but not working quite right

--- TCP_packet_verifier/tcp_packet_verifier.py
+++ TCP_packet_verifier/tcp_packet_verifier.py
@@ -1,16 +1,18 @@
-
+import os
 
 # Converts a dot delimited ip address to bytes
 def ip_to_bytes(address:str) -> bytes:
     return b''.join([int(x).to_bytes() for x in address.split(".")])
 
 def get_tcp_data(file_name:str) -> bytes:
-    with open(file_name, "rb") as fp:
+    file_path = os.path.join("tcp_data", file_name)
+    with open(file_path, "rb") as fp:
         tcp_data = fp.read()
     return tcp_data
 
 def get_source_and_destination_ip(file_name:str) -> list[str]:
-    with open(file_name, 'r') as file:
+    file_path = os.path.join("tcp_data", file_name)
+    with open(file_path, 'r') as file:
         first_line = file.readline()
 
     return first_line.split() 
@@ -21,16 +23,36 @@ def construct_pseudo_header(source_ip:str, destination_ip:str, tcp_length:int) -
     zero = b'\x00'
     protocol = b'\x06'
     bytes_needed = (tcp_length.bit_length() + 7) // 8
-    return source_ip_bytes + destination_ip_bytes + zero + protocol + tcp_length.to_bytes(bytes_needed)
+    return source_ip_bytes + destination_ip_bytes + zero + protocol + tcp_length.to_bytes(bytes_needed, 'big')
+
+def checksum(psuedo_header: bytes, tcp_zero_cksum: bytes)-> int:
+    if len(tcp_zero_cksum) % 2 == 1:
+        tcp_zero_cksum += b'\x00'
+    
+    data = psuedo_header + tcp_zero_cksum 
+    total = 0
+    for i in range(0,len(data),2):
+        word = int.from_bytes(data[i:i+2], "big")
+        
+        total += word
+        total = (total & 0xffff) + (total >> 16)
+        
+    return (~total) & 0xffff 
+
 
 
 def main(ip_file_name:str, tcp_data_file_name:str):
     source_ip_address, destination_ip_address = get_source_and_destination_ip(ip_file_name)
     tcp_data = get_tcp_data(tcp_data_file_name)
     pseudo_header = construct_pseudo_header(source_ip_address,destination_ip_address, len(tcp_data)) 
-     
+    original_cksum = int.from_bytes(tcp_data[16:18], 'big')
 
-
+    tcp_zero_cksum = tcp_data[:16] + b'\x00\x00' + tcp_data[18:]
+    
+    calculated_cksum = checksum(pseudo_header,tcp_zero_cksum)
+    
+    if calculated_cksum == original_cksum: return "PASS"
+    else: return "FAIL"
 
 
 def test():
@@ -56,6 +78,17 @@ def test():
     else:
         print("Failed!")
         print(f"expected: {expected_output}, recieved: {result}")
+
+    print("Running file tests...")
+    tcp_adder = "tcp_addrs_"
+    tcp_data = "tcp_data_"
+    for i in range(10):
+        adder_file = tcp_adder + str(i) + ".txt"
+        data_file = tcp_data + str(i) + ".dat"
+        result = main(adder_file, data_file)
+        if (i <= 4 and result == "PASS") or (i > 4 and result == "FAIL"): print("Passed!")
+        else:
+            print("Failed!")
 
 test()
 
